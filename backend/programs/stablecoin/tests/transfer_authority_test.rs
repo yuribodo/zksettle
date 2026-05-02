408bfa0bbb8013008583a0413682d8a2991775d6
Dnreikronos <dnreikronos2@gmail.com>
2026-05-02 19:43:54 -0300

    Deduplicate transfer_authority_test with shared helpers
    Extract transfer_admin_to_new and assert_rejects_default_pubkey to
    eliminate repeated admin-transfer setup and default-pubkey validation
    blocks.

--- backend/programs/stablecoin/tests/transfer_authority_test.rs
+++ backend/programs/stablecoin/tests/transfer_authority_test.rs
@@ -7,18 +7,19 @@ use solana_keypair::Keypair;
 use solana_signer::Signer;
 use stablecoin::error::StablecoinError;
 
+fn transfer_admin_to_new(svm: &mut litesvm::LiteSVM, admin: &Keypair, mint: &Pubkey) -> Keypair {
+    let new_admin = Keypair::new();
+    svm.airdrop(&new_admin.pubkey(), 1_000_000_000).unwrap();
+    let ix = transfer_authority_ix(&admin.pubkey(), mint, Some(new_admin.pubkey()), None);
+    send_tx(svm, &[ix], admin, &[admin]).expect("transfer admin should succeed");
+    new_admin
+}
+
 #[test]
 fn transfer_admin() {
     let TestEnv { mut svm, admin, mint_kp, .. } = setup();
 
-    let new_admin = Keypair::new();
-    let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        Some(new_admin.pubkey()),
-        None,
-    );
-    send_tx(&mut svm, &[ix], &admin, &[&admin]).expect("transfer admin should succeed");
+    let new_admin = transfer_admin_to_new(&mut svm, &admin, &mint_kp.pubkey());
 
     let treasury = read_treasury(&svm, &mint_kp.pubkey());
     assert_eq!(treasury.admin, new_admin.pubkey());
@@ -30,12 +31,7 @@ fn transfer_operator() {
     let TestEnv { mut svm, admin, mint_kp, .. } = setup();
 
     let new_operator = Keypair::new();
-    let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        None,
-        Some(new_operator.pubkey()),
-    );
+    let ix = transfer_authority_ix(&admin.pubkey(), &mint_kp.pubkey(), None, Some(new_operator.pubkey()));
     send_tx(&mut svm, &[ix], &admin, &[&admin]).expect("transfer operator should succeed");
 
     let treasury = read_treasury(&svm, &mint_kp.pubkey());
@@ -50,10 +46,8 @@ fn transfer_both() {
     let new_admin = Keypair::new();
     let new_operator = Keypair::new();
     let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        Some(new_admin.pubkey()),
-        Some(new_operator.pubkey()),
+        &admin.pubkey(), &mint_kp.pubkey(),
+        Some(new_admin.pubkey()), Some(new_operator.pubkey()),
     );
     send_tx(&mut svm, &[ix], &admin, &[&admin]).expect("transfer both should succeed");
 
@@ -69,12 +63,7 @@ fn transfer_rejects_non_admin() {
     let attacker = Keypair::new();
     svm.airdrop(&attacker.pubkey(), 1_000_000_000).unwrap();
 
-    let ix = transfer_authority_ix(
-        &attacker.pubkey(),
-        &mint_kp.pubkey(),
-        Some(attacker.pubkey()),
-        None,
-    );
+    let ix = transfer_authority_ix(&attacker.pubkey(), &mint_kp.pubkey(), Some(attacker.pubkey()), None);
     let result = send_tx(&mut svm, &[ix], &attacker, &[&attacker]);
     assert_anchor_error(result, ANCHOR_ERROR_CODE_OFFSET + StablecoinError::UnauthorizedAdmin as u32);
 }
@@ -88,48 +77,27 @@ fn transfer_rejects_no_change() {
     assert_anchor_error(result, ANCHOR_ERROR_CODE_OFFSET + StablecoinError::NoAuthorityChange as u32);
 }
 
+fn assert_rejects_default_pubkey(new_admin: Option<Pubkey>, new_operator: Option<Pubkey>, expected: StablecoinError) {
+    let TestEnv { mut svm, admin, mint_kp, .. } = setup();
+    let ix = transfer_authority_ix(&admin.pubkey(), &mint_kp.pubkey(), new_admin, new_operator);
+    let result = send_tx(&mut svm, &[ix], &admin, &[&admin]);
+    assert_anchor_error(result, ANCHOR_ERROR_CODE_OFFSET + expected as u32);
+}
+
 #[test]
 fn transfer_rejects_default_pubkey_admin() {
-    let TestEnv { mut svm, admin, mint_kp, .. } = setup();
-
-    let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        Some(Pubkey::default()),
-        None,
-    );
-    let result = send_tx(&mut svm, &[ix], &admin, &[&admin]);
-    assert_anchor_error(result, ANCHOR_ERROR_CODE_OFFSET + StablecoinError::InvalidNewAdmin as u32);
+    assert_rejects_default_pubkey(Some(Pubkey::default()), None, StablecoinError::InvalidNewAdmin);
 }
 
 #[test]
 fn transfer_rejects_default_pubkey_operator() {
-    let TestEnv { mut svm, admin, mint_kp, .. } = setup();
-
-    let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        None,
-        Some(Pubkey::default()),
-    );
-    let result = send_tx(&mut svm, &[ix], &admin, &[&admin]);
-    assert_anchor_error(result, ANCHOR_ERROR_CODE_OFFSET + StablecoinError::InvalidNewOperator as u32);
+    assert_rejects_default_pubkey(None, Some(Pubkey::default()), StablecoinError::InvalidNewOperator);
 }
 
 #[test]
 fn new_admin_can_act_after_transfer() {
     let TestEnv { mut svm, admin, mint_kp, .. } = setup();
-
-    let new_admin = Keypair::new();
-    svm.airdrop(&new_admin.pubkey(), 1_000_000_000).unwrap();
-
-    let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        Some(new_admin.pubkey()),
-        None,
-    );
-    send_tx(&mut svm, &[ix], &admin, &[&admin]).unwrap();
+    let new_admin = transfer_admin_to_new(&mut svm, &admin, &mint_kp.pubkey());
 
     let ix = pause_ix(&new_admin.pubkey(), &mint_kp.pubkey());
     send_tx(&mut svm, &[ix], &new_admin, &[&new_admin]).expect("new admin should be able to pause");
@@ -138,17 +106,7 @@ fn new_admin_can_act_after_transfer() {
 #[test]
 fn old_admin_cannot_act_after_transfer() {
     let TestEnv { mut svm, admin, mint_kp, .. } = setup();
-
-    let new_admin = Keypair::new();
-    svm.airdrop(&new_admin.pubkey(), 1_000_000_000).unwrap();
-
-    let ix = transfer_authority_ix(
-        &admin.pubkey(),
-        &mint_kp.pubkey(),
-        Some(new_admin.pubkey()),
-        None,
-    );
-    send_tx(&mut svm, &[ix], &admin, &[&admin]).unwrap();
+    transfer_admin_to_new(&mut svm, &admin, &mint_kp.pubkey());
 
     let ix = pause_ix(&admin.pubkey(), &mint_kp.pubkey());
     let result = send_tx(&mut svm, &[ix], &admin, &[&admin]);
